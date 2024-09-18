3cbfe32ef059aaf9a31fba6fe35cb1158e03a90e
Abdel Allali <abdelnoura97@gmail.com>
2024-09-18 19:10:30 +0100

    Added JS functionality

--- RPSgame/index.js
+++ RPSgame/index.js
@@ -1,63 +1,56 @@
  //Create a variable of type ARRAY called options to store values "rock", "paper", "scissors".
  const options = ["rock", "paper", "scissors"];
- //Create a variable of type STRING called player which stores the input of the player.
- let player = "";
- //Create a variable of type STRING called bot which stores the input of the bot.
- let bot = "";
- 
- 
- 
- //Create a function which reads the players input and stores it in the player variable.
+
+
+
+ //Create a function which works out the user's input
  let userInput = () => {
- 
- 
-         let answer = prompt("Rock, Paper, Scissors.... Shoot! ").toLowerCase();
-         //Check if user has entered the correct value
-         //first use toLowerCase in order to make string consistent
-         if (options.includes(answer)){
- 			console.log("Player has shot: " + answer);
-                 return answer;
-         }else{
-                 console.log("Invalid Input");
-         }
+         //Get buttons
+         const rockBtn = document.getElementsByClassName("card")[0];
+         const paperBtn = document.getElementsByClassName("card")[1];
+         const scissorsBtn = document.getElementsByClassName("card")[2];
+
+         //add event listeners to buttons
+         rockBtn.addEventListener('click', () => handleUserChoice(options[0]));
+         paperBtn.addEventListener('click', () => handleUserChoice(options[1]));
+         scissorsBtn.addEventListener('click', () => handleUserChoice(options[2]));
  }
 
- 
- //Create a function which generates a random number between 0 and 2 which will dictate what option 
+//Create a function which generates a random number between 0 and 2 which will dictate what option 
  //the bot will pick. Then store the result in the bot variable.
- 
- 
- let botInput = () => {
- 				//Generate a random value between 0 and 2
-                 let randomVal = Math.floor((Math.random()*3));
- 				//Log the result in the console. 
-                 console.log("Bot has shot: " + options[randomVal]);
-				//Return the value as a string. 
-                 return options[randomVal];
-   
- 
-         };
- 
- 
- player = userInput();
- bot = botInput();
- 
- 
- //Create a function which works out the victor.
- 
- let score = () => {
- 		player;
- 		bot;
- 
-         if (player == bot){
-           console.log("Draw");
-         }else if ((player == "rock" && bot == "scissors") || 
- 		(player == "paper" && bot == "rock") || 
- 		(player == "scissors" && bot == "paper")){
-                                 console.log("player wins!");
-                         } else {
-                                 console.log("bot wins!");
-                        }
+
+ let generateBotChoice = () => {
+        //Generate a random value between 0 and 2
+        let randomVal = Math.floor((Math.random() * 3));
+        //Log the result in the console. 
+        console.log("Bot has shot: " + options[randomVal]);
+        //Return the value as a string. 
+        return options[randomVal];
+};
+
+
+
+ let handleUserChoice = (userChoice) => {
+        let botChoice = generateBotChoice();
+        compareChoices(userChoice, botChoice);
  };
- 
- score();
+
+
+
+ let compareChoices = (user, bot) => {
+
+        if (user == bot) {
+                alert("Draw");
+        } else if ((user == "rock" && bot == "scissors") ||
+                (user == "paper" && bot == "rock") ||
+                (user == "scissors" && bot == "paper")) {
+                alert("player wins!");
+        } else {
+                alert("bot wins!");
+        }
+};
+
+
+
+
+userInput();
